45c21536a9a5da3aae23b9e2f19c00abc2de09d7
kurlycore777 <49575655+kurlycore777@users.noreply.github.com>
2022-07-19 13:35:58 -0500

    Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//MUI
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+    return (
+        <>
+            <AppBar position="static" style={{ background: '#ffff', color: '#000' }}>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        YorkStyle
+                    </Typography>
+                    <Button color="inherit">Inicio</Button>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Header
